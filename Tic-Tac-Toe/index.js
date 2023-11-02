97171163d91104b35ed503549295f038a14b3c71
ariyaltharun <ariyaltharun@gmail.com>
2023-11-02 12:41:33 +0000

    Implement MiniMax Algo
    [ Lot of changes in one commit 🥲 ]
    * Implement getBoardValues to convert array of div elements to array[str]
    * Implement miniMax and aiPlayer functions
    * Hardcored value i.e
        HUMAN_PLAYER = false
        COMPUTER_PLAYER = false
        AI_PLAYER = true

--- Tic-Tac-Toe/index.js
+++ Tic-Tac-Toe/index.js
@@ -24,11 +24,14 @@ function update(value, i, j) {
     board[i][j].innerText = value;
 }
 
-function board_status(board) {
-    /* Checks the board_status of the board i.e X wins, O wins, draw
-    @Parameter : 
-    @return: X | O | draw
-    */
+
+function getBoardValues(board) {
+    /* Copies all the board values and returns the 2d array
+    @Parameters:
+        board: array of div elements
+    @Returns:
+        2d array values of board values
+     */
     let cur_board = [
         ['', '', ''],
         ['', '', ''],
@@ -38,6 +41,16 @@ function board_status(board) {
     for (let i=0; i<3; i++)
         for (let j=0; j<3; j++)
             cur_board[i][j] = board[i][j].innerText
+    
+    return cur_board
+}
+
+
+function board_status(cur_board) {
+    /* Checks the board_status of the board i.e X wins, O wins, draw
+    @Parameter : 
+    @return: X | O | draw
+    */
 
     // Horizontal check
     for (let i=0; i<3; i++)
@@ -98,6 +111,80 @@ function computerPlayer(value, i, j) {
 }
 
 
+function aiPlayer() {
+    /* AIplayer uses minimax algorithm to find bestmove and play */
+    let cur_board = getBoardValues(board)
+    let bestScore = Infinity
+    let bestMove = {i:0, j:0}
+    for (let i=0; i<3; i++)
+        for (let j=0; j<3; j++)
+            if (cur_board[i][j] === "") {
+                cur_board[i][j] = "O"
+                let score = miniMax(cur_board, true)
+                cur_board[i][j] = ""
+                if (score < bestScore) {
+                    bestMove.i = i
+                    bestMove.j = j
+                    bestScore = score
+                }
+            }
+    console.log(bestMove)
+    if (!board_status(cur_board))
+        board[bestMove.i][bestMove.j].innerText = "O"
+}
+
+
+function miniMax(cur_board, maximize) {
+    /* MiniMax: Two player game algorithm
+                where one what's to maximize
+                their score and other what's
+                to minimize their score
+    @Parameters:
+        cur_board(2DArray): current board state
+        maximize(Bool): To choose player turn
+    @Output:
+        score
+    Here,
+        X -> wants to maximize i.e.. 1
+        O -> wants to minimize i.e.. -1
+        draw -> worst case i.e.. 0
+    */
+    // Base condition
+    let _scores = {
+        "X": 1,
+        "O": -1,
+        "draw": 0
+    }
+    if (board_status(cur_board))
+        return _scores[board_status(cur_board)]
+    
+    // Relation
+    if (maximize) {
+        let bestScore = -Infinity
+        for (let i=0; i<3; i++)
+            for (let j=0; j<3; j++)
+                if (cur_board[i][j] === "") {
+                    cur_board[i][j] = "X"
+                    let score = miniMax(cur_board, false)
+                    cur_board[i][j] = ""
+                    bestScore = Math.max(score, bestScore)
+                }
+        return bestScore
+    } else {
+        let bestScore = Infinity
+        for (let i=0; i<3; i++)
+            for (let j=0; j<3; j++) 
+                if (cur_board[i][j] === "") {
+                    cur_board[i][j] = "O"
+                    let score = miniMax(cur_board, true)
+                    cur_board[i][j] = ""
+                    bestScore = Math.min(score, bestScore)
+            }
+        return bestScore
+    }
+}
+
+
 /* Main */
 var board = document.querySelectorAll(".box")
 board = reshape_to_2d(board)
@@ -110,7 +197,8 @@ var cur_player = PLAYER1;
 
 // Possible players
 var HUMAN_PLAYER = false
-var COMPUTER_PLAYER = true
+var COMPUTER_PLAYER = false
+var AI_PLAYER = true
 
 // Computer player stuffs
 var avail = []
@@ -131,10 +219,14 @@ for (let i=0; i<3; i++)
                 } else if (COMPUTER_PLAYER === true) {
                     update(PLAYER1, i, j)
                     computerPlayer("O", i, j)
-                } 
-
-                game_status = board_status(board)
+                } else if (AI_PLAYER) {
+                    update(PLAYER1, i, j)
+                    aiPlayer()
+                }
                 
+                let cur_board = getBoardValues(board)
+                game_status = board_status(cur_board)
+
                 if (game_status === PLAYER1) {
                     display_game_status.innerText = "🎉 X Wins 🎉"
                     game_end = true
